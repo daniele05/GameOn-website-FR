afc0140a7210699b75b4746aacf56317fee7f35a
konda <konda300@yahoo.fr>
2021-07-19 06:16:10 +0200

    modal close et champs button radio et champs conditions obligatoires et conditions faculatives et button final

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,4 +1,6 @@
 
+
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -9,37 +11,37 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
+
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalbg = document.querySelector(".bground");
+const modalClose = document.getElementById("close");
+const validation = document.getElementById("btn-submit");
 
 
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
+console.log(modalBtn)
+
+// launch and close the modal event
+modalBtn.forEach((btn) => btn.addEventListener("click",launchModal));
+modalClose.addEventListener("click", close);
+// validation.addEventListener("click", validate);  
+
+// launch and close modal form
 function launchModal() {
+  console.log('ok')
+  
   modalbg.style.display = "block";
+
 }
+// const modalClose = document.getElementsByClassName(".close");
+function close(){
 
-//Close the modal form 
-function twFermer(){
-  window.close()
+  modalbg.style.display = "none";
 }
-
-// function afficher_cacher(id)
-// {
-//   if (document.getElementById(id).style.visibility=="hidden") {
-//     document.getElementById(id).style.visibility="visible";
-//     document.getElementById('close').innerHTML="cacher l'icone de fermeture";
-//   } 
-//   else{
-//     document.getElementById(id).style.visibility="hidden"; 
-//     document.getElementById('close').innerHTML="afficher l'icone de fermeture";
-//   }
-//   return true;
-// }
-
+function validate(){
+  
+}
 
 
 
@@ -124,9 +126,8 @@ const first = document.getElementById("first")
 
 //  //**********Validation email ******/
 
-let span= document.getElementsByTagName('span');
-
 email.onkeydown = function(){
+  // Creation de la Reg pour email
   const regex = /^([\.\_a-zA-Z0-9]+)@([a-zA-Z]+)\.([a-zA-Z]){2,8}$/;
   const regexo = /^([\.\_a-zA-Z0-9]+)@([a-zA-Z]+)\.([a-zA-Z]){2,3}$/;
   if (regex.test(email.value) || regexo.test(email.value)) 
@@ -139,39 +140,41 @@ email.onkeydown = function(){
    } else
    {
     let small = email.nextElementSibling;
-    small.innerHTML = 'le champs ne doit pas etre vide et email doit etre valide';
+    small.innerHTML = 'Email non valide';
     small.classList.add('text-danger');
    }
    
 }
 
-// Validation date de naissance 
+// Declaration de la date
+const birthdate = document.getElementById('birthdate');
 
- const birthdate = document.getElementById("birthdate")
-  function exactNumber(input){
-    if (input.value.lenght===8) {
-      return false
-    } else{
-      return true
-    }
-  }
-  last.addEventListener('focusout', function(){
-  if(isEmpty(birthdate)||!exactNumber(birthdate)){
-  
-     // Recuperation de la balise small
-    let small = birthdate.nextElementSibling;
-    small.innerHTML = 'Date Valide';
-    small.classList.add('text-danger')
-  
-  } else{
-    // Recuperation de la balise small
 
-    let small = birthdate.nextElementSibling;
-    small.innerHTML = 'Date Non Valide';
-    small.classList.add('text-success');
-  }
-  }); 
-  console.log(birthdate.value)
+// Ecouter la modif de la date
+birthdate.addEventListener = ('focusout', function () {
+  // Creation de la reg pour validation date
+  console.log('birthdate');
+   const birthdateReg=  /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]|(?:Jan|Mar|May|Jul|Aug|Oct|Dec)))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2]|(?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec))\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)(?:0?2|(?:Feb))\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9]|(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
+
+    if(birthdateReg(birthdate.value)){
+      let small = birthdate.nextElementSibling;
+      small.innerHTML = ' format de date de naissance valide';
+      small.classList.add('text-success');
+      
+
+        
+      } else{
+        // console.log('false')
+        let small = birthdate.nextElementSibling;
+        small.innerHTML = 'Date de naissance non valide';
+        small.classList.add('text-danger');
+      //  return  small.innerHTML = '  Format de date de naissance non valide';
+       
+      }
+    });
+  
+   
+
 
   // Validation Quantité de tournois effectués
 
@@ -179,64 +182,64 @@ email.onkeydown = function(){
 //  .innerHTML = localStorage.getItem("quantity");
 //  localStorage.getItem("quantity");
  // validation de la quantité de tournois effectée ( non vide, minimum 0 maximum 99)
- function isEmpty(input){
-  if(input.value===""){
-    return true
-  }else{
-    return false
-  }
-};
- function exactNumber(input){
-   if (input.value.length <="99") {
+
+ function exactNumbers(input){
+   if (input.value.length <= 99) {
      return true
    } else{
      return false
    }
  }
  quantity.addEventListener('focusout', function(){
- if(isEmpty(quantity)||!exactNumber(quantity)){
+ if(exactNumbers(quantity)){
    alert('le champs ne doit pas etre vide, doit comporter des caractères compris entre 0 et 99')
 
     // Recuperation de la balise small
 
    let small = quantity.nextElementSibling;
    small.innerHTML = 'Quantite Valide';
-   small.classList.add(text-success);
-   small.classList.color('lime');
+   small.classList.add('text-success');
+  
+  
     
   //  small.classList.remove(text-danger);
  
  } else{
     let small = quantity.nextElementSibling;
     small.innerHTML = 'Quantite non Valide';
-    small.classList.remove(text-danger);
-    small.classList.color('red');
+   
+    small.classList.add('text-danger');
+   
     
     
-    // small.classList.remove(text-success);
+     small.classList.remove(text-success);
  }
  }); 
 
  //Validation button radio
 
+ function validateFormData(){
+   var radios = document.getElementsByName("location");
+   var formValid = false; 
 
- document.querySelector('formData').addEventListener('change', function(){
+   var i= 0; 
+   while (!formValid && i < radios.length) {
+     if (radios[i].checked) formValid = true;
+     i++;
+   }
+
+   if (!formValid) alert("Must check some option!");
+   return formValid;
+ }
+
   
-  const locations = document.querySelectorAll('input'); 
-
-  for (var i = 0; i < locations.length; i++) {
-    if (locations[i].checked) break; 
-  }
-  console.log('locations[i].value');
-  })
- 
- 
-// Validation conditions formulaire
+// Validation conditions de formulaire
+//Conditions obligatoires
 
  const checkbox1 = document.getElementById("checkbox1")
 //  .innerHTML = localStorage.getItem("checkbox1");
 //  localStorage.getItem("checkbox1");
- function usingConditions(input){
+ function obligatoriesConditions(input){
   if(input.value===""){
     return false
   }else{
@@ -244,45 +247,69 @@ email.onkeydown = function(){
   }
 };
  
- usingConditions.addEventListener('focusout', function(){
- if(usingConditions(checkbox1)){
-   alert('le champs doit etre coché')
-   small.innerHTML = 'Conditions non Valides';
-   small.classList.add(text-danger);
- 
- } else{
-   
-   small.innerHTML = 'Conditions Valides';
-    small.classList.add(text-success);
-
-    // Recuperation de la balise small
+ checkbox1.addEventListener('focusout', function(){
+  console.log(checkbox1.value)
+ if(obligatoriesConditions(checkbox1)){
+   alert('le champs non vide et  doit absolument être coché')
+   // Recuperation de la balise small
 
    let small = checkbox1.nextElementSibling;
-  
+   small.innerHTML = 'Conditions Valides';
+   small.classList.add(text-success);
+   return true;
+ 
+ } else{
+   // Recuperation de la balise small
+
+   let small = checkbox1.nextElementSibling;
+   
+   small.innerHTML = 'Conditions non Valides';
+    small.classList.add(text-danger);
+    return false; 
  }
  }); 
 
- console.log(checkbox1.value)
-
- const checkbox2 = document.getElementById("checkbox2")
-//  .innerHTML = localStorage.getItem("checkbox2"); ;
-//   localStorage.getItem("checkbox2");
- if(isEmpty(checkbox2)||!nextEvents(checkbox2)){
-  alert('le champs peut être coché ou non')
-  small.innerHTML = 'Case cochée';
-} else{
-  small.innerHTML = 'Case non cochée';
-
+//Validation des consitions de formulaire
+//Conditions facultatives
+const checkbox2 = document.getElementById("checkbox2")
+//  .innerHTML = localStorage.getItem("checkbox2");
+//  localStorage.getItem("checkbox1");
+ function facultativeConditions(input){
+  if(input.value===""){
+    return true
+  }else{
+    return true
+  }
+};
+ 
+ checkbox2.addEventListener('focusout', function(){
+  console.log(checkbox2.value)
+ if(facultativeConditions(checkbox2)){
+   alert('le champs peut etre vide ou coché')
    // Recuperation de la balise small
 
-  let small = checkbox2.nextElementSibling;
+   let small = checkbox2.nextElementSibling;
+   small.innerHTML = 'Conditions Valides';
+   small.classList.add(text-success);
+   return true;
  
-}
+ } else{
+   // Recuperation de la balise small
+
+   let small = checkbox1.nextElementSibling;
+   
+   small.innerHTML = 'Conditions non Valides';
+    small.classList.add(text-success);
+    return true; 
+ }
+ }); 
+
 
 // Validation du btn final formulaire
 
- const btnsubmit = document.getElementById("btn-submit").addEventListener('submit', function() {
-   var error; 
+ document.getElementById("btn-submit").addEventListener("submit", validate);
+function validate() {
+  var error; 
    var inputs = document.getElementById("btn-submit").getElementsByTagName("input");
    for (var i = 0; i < inputs.lenght; i++) {
      console.log(inputs[i])
@@ -291,11 +318,17 @@ email.onkeydown = function(){
        
      }
    }
-
    if (error) {
     e.preventDefault();
     document.getElementsById("error").innerHTML = erreur;
     return false; 
      } else{
-    alert('Formulaire envoyé!');}
-     });
+    alert('Formulaire envoyé!');
+  }
+}
+     
+    
+ 
+   
+
+   
